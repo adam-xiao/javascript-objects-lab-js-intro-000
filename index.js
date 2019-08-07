be97e7fd37e07104e60bc081129936ec5caf5982
Adam Xiao <adam.x.xiao@gmail.com>
2019-08-07 00:18:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  return recipes[key] = value;
+  return object[key] = value;
 }
